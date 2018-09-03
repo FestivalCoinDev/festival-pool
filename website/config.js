8b5438dd1f59e95a6ae78fc70d1a445f3333dd60
FestivalCoinDev <taha.aarian@gmail.com>
2018-09-03 17:57:57 +0430

    new pool version:CN-FEST

---
+++ website/config.js
@@ -0,0 +1,25 @@
+var api = "http://80.211.166.244:8117";
+
+var api_blockexplorer = "";
+
+var poolHost = "80.211.166.244";
+
+var irc = "";
+
+var email = "taha.aarian@gmail.com";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "fest": [
+		["80.211.166.244", "http://80.211.166.244:8117"]
+    ]
+};
